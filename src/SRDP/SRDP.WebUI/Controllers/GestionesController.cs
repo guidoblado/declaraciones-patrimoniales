d6a3f7f2ab557112458af7eb40ec21fc6711a352
guidoblado <guidoblado@gmail.com>
2019-03-06 23:07:38 -0400

    Implementa add gestion

--- src/SRDP/SRDP.WebUI/Controllers/GestionesController.cs
+++ src/SRDP/SRDP.WebUI/Controllers/GestionesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using SRDP.Application.UseCases;
 using SRDP.Application.UseCases.GetGestiones;
+using SRDP.Application.UseCases.UpdateGestiones;
 using SRDP.WebUI.Models;
 using System;
 using System.Collections.Generic;
@@ -14,10 +15,12 @@ namespace SRDP.WebUI.Controllers
     public class GestionesController : Controller
     {
         private readonly IGetGestionesUserCase _getGestionesUserCase;
+        private readonly IUpdateGestionesUserCase _updateGestionesUserCase;
 
-        public GestionesController(IGetGestionesUserCase getGestionesUserCase)
+        public GestionesController(IGetGestionesUserCase getGestionesUserCase, IUpdateGestionesUserCase updateGestionesUserCase)
         {
             _getGestionesUserCase = getGestionesUserCase;
+            _updateGestionesUserCase = updateGestionesUserCase;
         }
         // GET: Gestiones
         public async Task<ActionResult> Index()
@@ -25,6 +28,27 @@ namespace SRDP.WebUI.Controllers
             var outputList = await _getGestionesUserCase.ExecuteList();
             var modelView = Mapper.Map<ICollection<GestionOutput>, List<GestionModel>>(outputList);
             return View(modelView);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Add()
+        {
+            var siguienteGestion = await _getGestionesUserCase.SiguienteGestion();
+            var modelView = new GestionModel
+            {
+                Gestion = siguienteGestion,
+                FechaInicio = new DateTime(siguienteGestion, 1, 1),
+                FechaFinal = new DateTime(siguienteGestion, 12, 31),
+                Vigente = false
+            };
+            return PartialView(modelView);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(GestionModel gestion)
+        {
+            await _updateGestionesUserCase.Add(gestion.Gestion, gestion.FechaInicio, gestion.FechaFinal, gestion.Vigente);
+            return Json(new { success = true, message = "Nueva gestión creada correctamente" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
