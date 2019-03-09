6629d1bcbf36378061051bf7c3de0b1ed238fd39
guidoblado <guidoblado@gmail.com>
2019-03-09 19:49:22 -0400

    Add User Profile

---
+++ src/SRDP/SRDP.Application/UseCases/UpdateProfile/IUpdateProfileUserCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRDP.Application.UseCases.UpdateProfile
+{
+    public interface IUpdateProfileUserCase
+    {
+        Task AddToAdmin(string userName, int funcionarioID);
+        Task DeleteFromAdmin(string userName);
+    }
+}
---
+++ src/SRDP/SRDP.WebUI/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SRDP.WebUI.Models
+{
+    public class MessageModel
+    {
+        public string CssClassName { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
