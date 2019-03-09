c723e99cdeaa4e74d1d85350b6611cf73017a346
guidoblado <guidoblado@gmail.com>
2019-03-09 08:29:40 -0400

    Add Authorization Attribute

---
+++ src/SRDP/SRDP.Application/Repositories/IRolesUsuarioWriteOnlyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRDP.Application.Repositories
+{
+    public interface IRolesUsuarioWriteOnlyRepository
+    {
+        Task Add(string userName, int funcionarioID);
+        Task Delete(string username);
+    }
+}
---
+++ src/SRDP/SRDP.WebUI/App_Start/AuthorizeRolesEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SRDP.WebUI.App_Start
+{
+    public enum Roles
+    {
+        Usuario,
+        Administrador
+    }
+}
